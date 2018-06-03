e48a2bb623d610b04373e2f875dc3718ae7f8a7a
Pam De Silva <pam.desilva@gmail.com>
2018-06-03 16:35:02 +0100

    Post across to the updated tutorial code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+import { createStore } from 'redux';
+
+const defaultState = {
+  appName: 'conduit',
+  articles: null
+};
+
+const reducer = function(state = defaultState, action) {
+  return state;
+};
+
+const store = createStore(reducer);
+
+ReactDOM.render((
+  <Provider store={store}>
+    <App />
+  </Provider>
+), document.getElementById('root'));
